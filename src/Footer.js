4156ef0d1b1e8b3f4b54d52e1c9ff3bcda5a7f50
Ced.dev <111016808+Cedrick-KC@users.noreply.github.com>
2025-12-23 14:53:22 +0200

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="app-footer">
+      <p>© 2025 Cedrique's Object Detector | Made with 💙 </p>
+      <p>Best for SmartPhones which are good for ML</p>
+    </footer>
+  );
+}
+
+export default Footer;
